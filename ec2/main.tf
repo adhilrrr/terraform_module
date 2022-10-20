fd97cce123c3a457cabac0720c8f97af011ea7b7
adhilrrr <113431192+adhilrrr@users.noreply.github.com>
2022-10-20 08:38:36 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -23,7 +23,6 @@ resource "aws_instance" "web" {
   count = var.instance_count
   ami             = data.aws_ami.ubuntu.id
   instance_type   = var.instance_type
-  security_groups = [aws_security_group.allow_tls.name]
     root_block_device {
     volume_size = var.instance_root_device_size
     volume_type = "gp2"
