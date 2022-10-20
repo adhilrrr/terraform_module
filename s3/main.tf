cb652351bc7b25ac7eece2a7645d40c0ab11595f
adhilrrr <113431192+adhilrrr@users.noreply.github.com>
2022-10-20 08:00:23 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,12 +1,12 @@
 
-resource "aws_s3_bucket" "adhil_devs_s3" {
+resource "aws_s3_bucket" "zxc_devs3" {
 
   bucket                = var.bucket
   #bucket_prefix         = var.bucket_prefix
   force_destroy         = var.force_destroy
 
   tags = {
-    Name        = "adhil_devs_s3"
+    Name        = "zxc_devs3"
   }
 }
 
