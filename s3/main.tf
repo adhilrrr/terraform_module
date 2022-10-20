18f4c13219e3ecc8007caa107163a92687751296
adhilrrr <113431192+adhilrrr@users.noreply.github.com>
2022-10-20 08:16:38 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "devs389" {
   bucket                = var.bucket
   #bucket_prefix         = var.bucket_prefix
   force_destroy         = var.force_destroy
-  object_lock_enabled   = var.object_lock_enabled
 
   tags = {
     Name        = "devonS389"
